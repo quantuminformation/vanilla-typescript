ee63eb4b7f9b92be4b31ebe862aa460f32c0ce57
Nikos Katsikanis <nkatsikanis@gmail.com>
2017-02-01 23:04:48 +0000

    Start events

---
+++ events/ApplicationEvents.ts
@@ -0,0 +1,9 @@
+/**
+ * Common Application Events
+ */
+
+export class ApplicationEvents {
+
+  static readonly USER_HAS_LOGGED_IN = "USER_HAS_LOGGED_IN"
+  static readonly USER_HAS_LOGGED_OUT = "USER_HAS_LOGGED_OUT"
+}
