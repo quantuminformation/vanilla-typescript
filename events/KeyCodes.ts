bb1cc89485f3d2e81ff12133848d45b3a1041521
Nikos Katsikanis <nkatsikanis@gmail.com>
2017-02-25 11:24:37 +0000

    type to numbers

--- events/KeyCodes.ts
+++ events/KeyCodes.ts
@@ -3,21 +3,24 @@
 export let KeyCodes = {
   BACKSPACE: 8,
 
-  RETURN: "13",
-  ESC: "27",
-  SPACE: "32",
+  RETURN: 13,
+  ESC: 27,
+  SPACE: 32,
 
-  LEFT: "37",
-  UP: "38",
-  RIGHT: "39",
-  DOWN: "40",
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40,
 
-  ZERO: "48",
-  ONE: "49",
-  TWO: "50",
+  ZERO: 48,
+  ONE: 49,
+  TWO: 50,
 
-  b: "66",
-  F: "70",
-  k: "75",
-  m: "77"
+  a: 65,
+  b: 66,
+  F: 70,
+  k: 75,
+  m: 77,
+
+  WIN_or_CMD:91  //to detect cmd on key up use this, on keydown you can use event.metaKey
 }
